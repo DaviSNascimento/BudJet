9d7f86cd8d48f582cfa3f9b7ae1df9336c7f42e7
DaviSNascimento <nascimentoos.davi@gmail.com>
2026-01-15 17:14:43 -0300

    feature: criando UI dashboard (incompleto)

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -2,16 +2,33 @@ import { createSupabaseServer } from "@/src/lib/supabase/server";
 import { redirect } from "next/navigation";
 import styles from "./custom.module.css"
 import clsx from "clsx";
+import Image from "next/image";
+import { Navbar } from "@/src/components/Navbar/navigationbar";
+import { CircleUser } from "lucide-react";
 
 export default async function Dashboard() {
   const supabase = await createSupabaseServer();
-  const { data } = await supabase.auth.getUser();
+  const { data: { user } } = await supabase.auth.getUser();
 
-  if (!data.user) redirect("/login");
+  if (!user) redirect("/login");
+
+  const fullName = user.user_metadata.full_name ?? "Usuário";
+
   return (
-    <div className={clsx(styles.body, "")}>
-      <h1 className="text-3xl">Bem-vindo {data.user.email}</h1>
-      <button className=" rounded-xl text-4xl font-semibold border-2 p-2 bg-green-700 m-10"><a href="/budget">Criar Orçamento</a></button>
+    <div className={clsx(styles.body, "min-h-screen")}>
+      <nav className="flex items-center px-5 py-3 border-b-2">
+        <div className="flex items-center gap-2">
+          <Image src="./logo.svg" width={1} height={1} alt="BudJet-logo" className="w-8 mr-2" />
+          <h1 className="font-bold text-2xl text-stone-800">BudJet</h1>
+        </div>
+        <div className="flex-1 flex items-center justify-center">
+          <Navbar className="rounded-2xl" />
+        </div>
+        <div>
+          <CircleUser className=" h-7 w-7 text-stone-600"/>
+        </div>
+      </nav>
+      <h1 className="text-3xl font-bold pt-5 pl-5 text-olive-leaf-900">Bem-vindo <span className="text-olive-leaf-600">{fullName}</span> 👋</h1>
     </div>
   )
 }
